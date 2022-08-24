745724e691f098d9d2c71fcebd276186456fd080
Jonatas_Amorim <jonatas231997@live.com>
2022-08-24 15:23:14 -0300

    data base with sqlite

---
+++ src/routes/users.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const UsersController = require("../controllers/UsersController");
+const usersRoutes = Router();
+
+const usersController = new UsersController();
+
+/* function myMiddleware(request, response, next) {
+    console.log("Você passou pelo Middleware");
+
+    if (!request.body.isAdmin) {
+        return response.json({ message: "user unauthorized" });
+    }
+
+    next();
+} */
+
+usersRoutes.post("/",  usersController.create);
+
+module.exports = usersRoutes;
