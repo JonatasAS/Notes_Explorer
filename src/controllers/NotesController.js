4d6d8d1faf1b55ecb821a7afdafa9b1cb7433cb9
Jonatas Amorim Silva <jonatas231997@live.com>
2022-09-05 22:15:54 -0300

    like operator

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -58,7 +58,7 @@ class NotesController {
     async index(request, response) {
         const { user_id } = request.query;
 
-        const notes = await knex("notes").where({user_id}).orderBy("title");
+        const notes = await knex("notes").where({user_id}).whereLike("title", `${title}$` ).orderBy("title");
 
         return response.json(notes);
     }
