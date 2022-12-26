81258d8c26a3f091bd6723d6faee39a910e7e024
Jonatas Amorim Silva <jonatas231997@live.com>
2022-12-25 21:40:15 -0300

    Configuration token for all routes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const NotesController = require("../controllers/NotesController");
 const notesRoutes = Router();
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const notesController = new NotesController();
 
-notesRoutes.post("/:user_id",  notesController.create);
+notesRoutes.use(ensureAuthenticated);
+
+notesRoutes.post("/",  notesController.create);
 notesRoutes.get("/:id",  notesController.show);
 notesRoutes.delete("/:id",  notesController.delete);
 notesRoutes.get("/",  notesController.index);
